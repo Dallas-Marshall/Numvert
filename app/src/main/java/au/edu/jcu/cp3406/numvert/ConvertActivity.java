ae6d52234f79212e16d8c7d9124ca5e8b9ef3955
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-21 20:29:45 +1000

    Add Intents to switch between settings and convert activities.

--- app/src/main/java/au/edu/jcu/cp3406/numvert/ConvertActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/numvert/ConvertActivity.java
@@ -1,5 +1,6 @@
 package au.edu.jcu.cp3406.numvert;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -88,5 +89,10 @@ public class ConvertActivity extends AppCompatActivity {
         fromUnitsDisplayText = "";
         isEntryFrozen = hasDecimalBeenEntered = false;
         updateFromUnitsEntry();
+    }
+
+    public void settingsClicked(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/au/edu/jcu/cp3406/numvert/SettingsActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/numvert/SettingsActivity.java
@@ -2,7 +2,9 @@ package au.edu.jcu.cp3406.numvert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void convertCLicked(View view) {
+        Intent intent = new Intent(this, ConvertActivity.class);
+        startActivity(intent);
     }
 }
