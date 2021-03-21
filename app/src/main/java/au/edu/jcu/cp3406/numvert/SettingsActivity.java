b971594802c6f28f5ce2a2af58186aa05cd3852a
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-21 21:11:00 +1000

    Add unit selection spinners and handlers

--- app/src/main/java/au/edu/jcu/cp3406/numvert/SettingsActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/numvert/SettingsActivity.java
@@ -1,21 +1,63 @@
 package au.edu.jcu.cp3406.numvert;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SettingsActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private String fromUnits;
+    private String toUnits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Setup spinners
+        Spinner fromUnitsSpinner = findViewById(R.id.fromUnitsSpinner);
+        Spinner toUnitsSpinner = findViewById(R.id.toUnitsSpinner);
+
+        ArrayAdapter<CharSequence> adapterFromUnits = ArrayAdapter.createFromResource(this, R.array.measurements, android.R.layout.simple_spinner_item);
+        adapterFromUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromUnitsSpinner.setAdapter(adapterFromUnits);
+
+        ArrayAdapter<CharSequence> adapterToUnits = ArrayAdapter.createFromResource(this, R.array.measurements, android.R.layout.simple_spinner_item);
+        adapterToUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toUnitsSpinner.setAdapter(adapterToUnits);
+
+        fromUnitsSpinner.setOnItemSelectedListener(this);
+        toUnitsSpinner.setOnItemSelectedListener(this);
     }
 
-    public void convertCLicked(View view) {
+    /**
+     * Handles event when convertButton is pressed.
+     *
+     * @param buttonPressed The convertButton view element.
+     */
+    public void convertCLicked(View buttonPressed) {
         Intent intent = new Intent(this, ConvertActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Handles when a spinner is changed.
+     */
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if (parent.getId() == R.id.fromUnitsSpinner) {
+            fromUnits = parent.getItemAtPosition(position).toString();
+        } else if (parent.getId() == R.id.toUnitsSpinner) {
+            toUnits = parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
