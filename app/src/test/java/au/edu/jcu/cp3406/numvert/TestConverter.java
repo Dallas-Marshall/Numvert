7a6a0a918688b3bb8dd751db0c140abfda555e79
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-25 11:29:43 +1000

    Rename TestConverter

---
+++ app/src/test/java/au/edu/jcu/cp3406/numvert/TestConverter.java
@@ -0,0 +1,26 @@
+//package au.edu.jcu.cp3406.numvert;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+///**
+// * Example local unit test, which will execute on the development machine (host).
+// *
+// * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+// */
+//public class TestConverter {
+//    private double fromMeasurement;
+//    private double conversionResult;
+//    private String
+//
+//    @Test
+//   public void testConversions() {
+//
+//    }
+//
+//    @Test
+//    public void testOutputs() {
+//        pass
+//    }
+//}
